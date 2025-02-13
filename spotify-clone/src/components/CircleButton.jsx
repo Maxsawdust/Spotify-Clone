90a5c8b8685c788a4078916a80a5dac307be8296
Max Saunders <102803082+Maxsawdust@users.noreply.github.com>
2025-02-13 12:56:20 +0000

    edit styling on header

--- spotify-clone/src/components/CircleButton.jsx
+++ spotify-clone/src/components/CircleButton.jsx
@@ -1,9 +1,14 @@
 import placeHolderSquare from "../assets/placeholder-square.jpg";
 import "../css/CircleButton.css";
 
+// CircleButton accepts props to manipulate img src, alt, and button backgroundColor
 export default function CircleButton(props) {
   return (
-    <button className="CircleButton">
+    <button
+      className="CircleButton"
+      style={{ backgroundColor: props.bgCol || "#1f1f1f" }}
+      id={props.id || ""}
+    >
       <img
         className="CircleButtonImg"
         src={props.src || placeHolderSquare}
--- spotify-clone/src/components/Header.jsx
+++ spotify-clone/src/components/Header.jsx
@@ -1,15 +1,25 @@
 import CircleButton from "./CircleButton";
+import spotifyLogo from "../assets/spotify-logo.png";
+import homeIcon from "../assets/home-icon.png";
+import profilePic from "../assets/profile-pic.jpg";
+import browseIcon from "../assets/browse-icon.png";
+import searchIcon from "../assets/search-icon.png";
+import notificationsBell from "../assets/notifications-bell.png";
 import "../css/Header.css";
 
 export default function Header() {
   return (
     <header>
-      <CircleButton />
+      <CircleButton src={spotifyLogo} />
 
       <div>
-        <CircleButton />
+        <CircleButton src={homeIcon} alt="home" />
         <div id="search-bar">
+          <div className="icon-container">
+            <img src={searchIcon} alt="search" id="search-icon" />
+          </div>
           <input type="text" id="search-song" />
+          <CircleButton src={browseIcon} alt="browse" id="browse-icon" />
         </div>
       </div>
 
@@ -18,9 +28,13 @@ export default function Header() {
           <a href=""></a>
         </div>
         <div id="notification-div">
-          <button></button>
+          <CircleButton
+            src={notificationsBell}
+            alt="notifications"
+            bgCol={"black"}
+          />
         </div>
-        <CircleButton />
+        <CircleButton src={profilePic} alt="view profile" />
       </div>
     </header>
   );
