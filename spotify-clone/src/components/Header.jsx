a2395f3c69cb8add942fc0b3b1ce52b06a459179
Max Saunders <102803082+Maxsawdust@users.noreply.github.com>
2025-02-14 13:18:11 +0000

    edit Header element

--- spotify-clone/src/components/Header.jsx
+++ spotify-clone/src/components/Header.jsx
@@ -5,33 +5,42 @@ import profilePic from "../assets/profile-pic.jpg";
 import browseIcon from "../assets/browse-icon.png";
 import searchIcon from "../assets/search-icon.png";
 import notificationsBell from "../assets/notifications-bell.png";
+import downloadIcon from "../assets/download-icon.png";
 import "../css/Header.css";
 
 export default function Header() {
   return (
     <header>
-      <CircleButton src={spotifyLogo} />
+      <CircleButton src={spotifyLogo} bgCol="black" />
 
-      <div>
+      <div id="center-header">
         <CircleButton src={homeIcon} alt="home" />
         <div id="search-bar">
           <div className="icon-container">
             <img src={searchIcon} alt="search" id="search-icon" />
           </div>
-          <input type="text" id="search-song" />
+          <input
+            type="text"
+            id="search-input"
+            placeholder="What do you want to play?"
+          />
           <CircleButton src={browseIcon} alt="browse" id="browse-icon" />
         </div>
       </div>
 
       <div>
         <div id="install-div">
-          <a href=""></a>
+          <a href="/" id="install-link">
+            <img src={downloadIcon} alt="donwload app" id="download-icon" />
+            <span>Install App</span>
+          </a>
         </div>
-        <div id="notification-div">
+        <div>
           <CircleButton
             src={notificationsBell}
             alt="notifications"
             bgCol={"black"}
+            id="notifications-button"
           />
         </div>
         <CircleButton src={profilePic} alt="view profile" />
